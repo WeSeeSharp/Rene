a0cfd57718527b3a567a523b740e956a3f2e0f97
Rene Rios <rrios@pillartechnology.com>
2018-06-07 16:09:39 -0500

    all

---
+++ LearningAboutMockTests/TestLearningEnum.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace LearningAboutMockTests
+{
+    enum DaysOfTheWeek
+    {
+        Sunday = 1,
+        Monday = 5,
+        Tuesday = 11,
+        Wednesday = 15,
+        Thursday = 21,
+        Friday = 25,
+        Saturday
+    };
+
+    enum DumbDaysOfTheWeek
+    {
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
+    };
+
+    class TestLearningEnum
+    {
+        [Test]
+        public void CanAccessEnum()
+        {
+//            Assert.AreEqual("Sunday", DaysOfTheWeek.Sunday);
+//            Assert.AreEqual(21, (int)DaysOfTheWeek.Thursday)));
+
+            Assert.AreEqual(0, (int)DumbDaysOfTheWeek.Sunday);
+
+            var a = DaysOfTheWeek.Friday;
+            if (a == DaysOfTheWeek.Friday)
+            {
+                Assert.AreEqual(a, DaysOfTheWeek.Friday);
+            }
+        }
+
+        [Test]
+        public void WhatEnumsTryToSolve()
+        {
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+            dictionary.Add("Sunday", 1);
+            dictionary.Add("Monday", 2);
+            dictionary.Add("Tuesday", 3);
+            dictionary.Add("Wednesday", 4);
+            dictionary.Add("Thursday", 5);
+            dictionary.Add("Friday", 6);
+            dictionary.Add("Saturday", 7);
+
+            var a = dictionary["Friday"];
+
+            if (a == 6)
+            {
+                Assert.AreEqual(a, 6);
+            }
+
+        }
+    }
+}
---
+++ LearningAboutMockTests/TestingOperatorOverloader.cs
@@ -0,0 +1,161 @@
+﻿using NUnit.Framework;
+
+namespace LearningAboutMockTests
+{
+    class Box
+    {
+        int Height { get; set; }
+        int Width { get; set; }
+        int Length { get; set; }
+
+        public Box(int x, int y, int z)
+        {
+            Height = x;
+            Width = y;
+            Length = z;
+        }
+
+        public int GetVolume()
+        {
+            return Height * Width * Length;
+        }
+    }
+
+    class Box2
+    {
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public int Length { get; set; }
+
+        public Box2()
+        {}
+
+        public Box2(int x, int y, int z)
+        {
+            Height = x;
+            Width = y;
+            Length = z;
+        }
+
+        public int GetVolume()
+        {
+            return Height * Width * Length;
+        }
+
+        public static Box2 operator +(Box2 b, Box2 c)
+        {
+            Box2 box = new Box2();
+            box.Length = b.Length + c.Length;
+            box.Width = b.Width + c.Width;
+            box.Height = b.Height + c.Height;
+            return box;
+        }
+
+        //Lets do a subtract operator
+        //public static Box2 operator -(Box2 b, Box2 c)
+        //{
+        //    Box2 box = new Box2();
+        //    return box;
+        //}
+
+        //Lets do a multiply operator
+        //public static Box2 operator *(Box2 b, Box2 c)
+        //{
+        //    Box2 box = new Box2();
+        //    return box;
+        //}
+
+        //Lets do a ++ operator
+        //public static Box2 operator -(Box2 b, Box2 c)
+        //{
+        //    Box2 box = new Box2();
+        //    return box;
+        //}
+
+        //Lets do a == operator
+//        public static bool operator ==(Box2 b, Box2 c)
+//        {
+//            return ((b.Length == c.Length) && (b.Width == c.Width) && (b.Height == c.Height));
+//        }
+
+        //public static bool operator !=(Box2 b, Box2 c)
+        //{
+        //    return ((b.Length == c.Length) && (b.Width == c.Width) && (b.Height == c.Height));
+        //}
+
+        public static Box2 operator -(Box2 firstBox, Box2 secondBox)
+        {
+            var subtractedBox = new Box2();
+            subtractedBox.Height = firstBox.Height - secondBox.Height;
+            subtractedBox.Width = firstBox.Width - secondBox.Width;
+            subtractedBox.Length = firstBox.Length - secondBox.Length;
+            return subtractedBox;
+        }
+        
+        
+        
+        
+        
+        
+    }
+
+    class TestingOperatorOverloader
+    {
+        [Test]
+        public void TestCantAddComplexObjects()
+        {
+            var box1 = new Box(1, 2, 3);
+            var box2 = new Box(4, 5, 6);
+
+//            var box3 = box1 + box2;
+        }
+
+        [Test]
+        public void TestCanAddComplexObjectsWhenYouOverloadTheOperatorInAComplexObject()
+        {
+            var box1 = new Box2(1, 2, 3);
+            var box2 = new Box2(4, 5, 6);
+
+            var box3 = box1 + box2;
+
+            var expectedValue = (1 + 4) * (2 + 5) * (3 + 6);
+
+            Assert.AreEqual(expectedValue, box3.GetVolume());
+        }
+        
+        [Test]
+        public void TestCanSubtractComplexObjectsWhenYouOverloadTheOperatorInAComplexObject()
+        {
+            var box1 = new Box2(1, 2, 3);
+            var box2 = new Box2(4, 5, 6);
+
+            var box3 = box2 - box1;
+
+            var expectedValue = (4 - 1) * (5 - 2) * (6 - 3);
+
+            Assert.AreEqual(expectedValue, box3.GetVolume());
+        }
+
+        [Test]
+        public void TestCanHaveEqualToComparison()
+        {
+            var box1 = new Box2(1, 2, 3);
+            var box3 = new Box2(1, 2, 3);
+
+            var expected = box1 == box3;
+
+            Assert.True(expected);
+        }
+
+        [Test]
+        public void TestCanHaveNotEqualToComparison()
+        {
+            var box1 = new Box2(1, 2, 3);
+            var box2 = new Box2(4, 5, 6);
+
+            var expected = box1 == box2;
+
+            Assert.False(expected);
+        }
+    }
+}
